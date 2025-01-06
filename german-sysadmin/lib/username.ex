b27069ca1d44c99bfdf7d9650aea3d1bf6aed8b3
Piotr Opała <opalapj@gmail.com>
2025-01-06 22:22:14 +0100

    'German Sysadmin' final

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,10 +1,33 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  @moduledoc """
+  Create sanitized aliases for all email accounts.
+  """
 
-    # Please implement the sanitize/1 function
+  # ä becomes ae
+  # ö becomes oe
+  # ü becomes ue
+  # ß becomes ss
+
+  @doc """
+  Sanitize existing usernames by removing everything but lowercase letters.
+  Allow underscores.
+  Substitute German characters.
+  """
+  @spec sanitize(charlist) :: charlist
+  def sanitize(username)
+  def sanitize([]), do: []
+
+  def sanitize([head | tail]) do
+    result =
+      case head do
+        x when (x >= ?a and x <= ?z) or x == ?_ -> [x]
+        ?ä -> ~c"ae"
+        ?ö -> ~c"oe"
+        ?ü -> ~c"ue"
+        ?ß -> ~c"ss"
+        _ -> ~c""
+      end
+
+    result ++ sanitize(tail)
   end
 end
