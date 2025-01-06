911ca25f7fbf7578e0df00fd80065ca2d70929ce
Piotr Opała <opalapj@gmail.com>
2025-01-06 12:16:19 +0100

    'Guessing Game' final

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,36 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+  def compare(secret_number, guess)
+    when
+      secret_number == guess
+    do
+    "Correct"
+  end
+
+  def compare(secret_number, guess)
+    when
+      abs(guess - secret_number) == 1
+    do
+    "So close"
+  end
+
+  def compare(secret_number, guess)
+    when
+      (guess - secret_number) > 1
+    do
+    "Too high"
+  end
+
+  def compare(secret_number, guess)
+    when
+      (guess - secret_number) < 1
+    do
+    "Too low"
+  end
+
+  def compare(_, guess \\ :no_guess)
+    when
+      guess == :no_guess
+    do
+    "Make a guess"
   end
 end
