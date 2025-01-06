19350c007dafd4def1c6392a8a4dfe29f12edc3e
Piotr Opała <opalapj@gmail.com>
2025-01-06 14:25:08 +0100

    'High School Sweetheart' final

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,42 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
     # Please implement the first_letter/1 function
+    name
+    |> String.trim()
+    |> String.first()
   end
 
   def initial(name) do
     # Please implement the initial/1 function
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
   end
 
   def initials(full_name) do
     # Please implement the initials/1 function
+    [fname, lname] = String.split(full_name, " ")
+    initial(fname) <> " " <> initial(lname)
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
     # Please implement the pair/2 function
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
