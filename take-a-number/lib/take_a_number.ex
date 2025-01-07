5966c3890cf682305c7158dc9643f9431ff1e727
Piotr Opała <opalapj@gmail.com>
2025-01-07 20:00:22 +0100

    'Take-A-Number' final

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,27 @@
 defmodule TakeANumber do
   def start() do
     # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  def loop(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        IO.inspect(state, label: "state")
+        loop(state)
+
+      {:take_a_number, sender_pid} ->
+        state = state + 1
+        send(sender_pid, state)
+        IO.inspect(state, label: "state")
+        loop(state)
+
+      :stop ->
+        nil
+
+      _ ->
+        loop(state)
+    end
   end
 end
