34cf4c086814c5b552f22e61ad8b3b09b6762eb3
Piotr Opała <opalapj@gmail.com>
2025-01-08 22:53:16 +0100

    'Basketball Website' final

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,19 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
     # Please implement the extract_from_path/2 function
+    keys = String.split(path, ".")
+    extract_from_keys(data, keys)
+  end
+
+  defp extract_from_keys(data, keys)
+  defp extract_from_keys(data, []), do: data
+  defp extract_from_keys(data, [first_key | rest]) do
+    extract_from_keys(data[first_key], rest)
   end
 
   def get_in_path(data, path) do
     # Please implement the get_in_path/2 function
+    keys = String.split(path, ".")
+    get_in(data, keys)
   end
 end
