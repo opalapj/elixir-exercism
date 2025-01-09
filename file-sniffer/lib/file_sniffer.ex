9bb53f89f0ff3ab978510c849953b1f135480a00
Piotr Opała <opalapj@gmail.com>
2025-01-09 21:23:15 +0100

    'File Sniffer' final

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,39 @@
 defmodule FileSniffer do
-  def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
-  end
+  def type_from_extension(extension)
+  def type_from_extension("exe"), do: "application/octet-stream"
+  def type_from_extension("bmp"), do: "image/bmp"
+  def type_from_extension("png"), do: "image/png"
+  def type_from_extension("jpg"), do: "image/jpg"
+  def type_from_extension("gif"), do: "image/gif"
+  def type_from_extension(_), do: nil
 
-  def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
-  end
+  def type_from_binary(file_binary)
+
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>),
+    do: "application/octet-stream"
+
+  def type_from_binary(<<0x42, 0x4D, _::binary>>),
+    do: "image/bmp"
+
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>),
+    do: "image/png"
+
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>),
+    do: "image/jpg"
+
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>),
+    do: "image/gif"
+
+  def type_from_binary(_), do: nil
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    from_binary = type_from_binary(file_binary)
+    from_extension = type_from_extension(extension)
+
+    if from_binary == from_extension and from_binary != nil do
+      {:ok, from_binary}
+    else
+      {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
