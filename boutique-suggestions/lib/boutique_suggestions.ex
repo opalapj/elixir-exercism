9faa7dbda11e3b028c667df8e97397d8f8a1ab84
Piotr Opała <opalapj@gmail.com>
2025-01-10 21:23:07 +0100

    'Boutique Suggestions' final

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,22 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
+  def get_combinations(tops, bottoms, options \\ []) do
     # Please implement the get_combinations/3 function
+
+    # My solution for price option.
+#    price = case options do
+#      [maximum_price: price] -> price
+#      _ -> 100
+#    end
+
+    # The best solution for price option.
+    price = Keyword.get(options, :maximum_price, 100)
+
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color,
+        (top.price + bottom.price) < price
+    do
+        {top, bottom}
+    end
   end
 end
