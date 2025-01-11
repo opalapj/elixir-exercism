0f1818f5f52c3b7f2d14c557c022edf78cd5fced
Piotr Opała <opalapj@gmail.com>
2025-01-11 23:43:51 +0100

    'RPN Calculator' final

--- rpn-calculator/lib/rpn_calculator.ex
+++ rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,26 @@
 defmodule RPNCalculator do
   def calculate!(stack, operation) do
     # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
     # Please implement the calculate/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      _ ->
+        :error
+        #      e -> {:error, e.message}
+    end
   end
 
   def calculate_verbose(stack, operation) do
     # Please implement the calculate_verbose/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      e in ArgumentError -> {:error, e.message}
+    end
   end
 end
