e05fa979f3dc5b25b877653e9e6f59a29769ff1f
Piotr Opała <opalapj@gmail.com>
2025-01-12 18:26:54 +0100

    'Stack Underflow' final

--- stack-underflow/lib/rpn_calculator/exception.ex
+++ stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,27 @@
 defmodule RPNCalculator.Exception do
   # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception [message: "division by zero occurred"]
+  end
 
   # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    @message "stack underflow occurred"
+
+    defexception [message: @message]
+
+    @impl true
+    def exception(value) do
+      case value do
+        [] -> %StackUnderflowError{}
+        _ -> %StackUnderflowError{message: "#{@message}, context: #{value}"}
+      end
+    end
+  end
+
+  def divide(stack)
+  def divide([]), do: raise(StackUnderflowError, "when dividing")
+  def divide([_]), do: raise(StackUnderflowError, "when dividing")
+  def divide([0, _]), do: raise(DivisionByZeroError)
+  def divide([divisor, dividend]), do: dividend / divisor
 end
