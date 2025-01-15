9c398282e7d760a4c191f5a5a757d303f9d8aec3
Piotr Opała <opalapj@gmail.com>
2025-01-15 19:18:15 +0100

    'Lucas Numbers' final

--- lucas-numbers/lib/lucas_numbers.ex
+++ lucas-numbers/lib/lucas_numbers.ex
@@ -5,7 +5,24 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
+  def generate(count)
+
+  def generate(count)
+      when not is_integer(count) or
+             (is_integer(count) and count < 1) do
+    raise ArgumentError, "count must be specified as an integer >= 1"
+  end
+
+  def generate(1), do: [2]
+  def generate(2), do: [2, 1]
+
   def generate(count) do
-    # Please implement the generate/1 function
+    tail =
+      generate(2)
+      |> Stream.iterate(fn [a, b] -> [b, a + b] end)
+      |> Stream.map(fn [_a, b] -> b end)
+      |> Enum.take(count - 1)
+
+    [2 | tail]
   end
 end
