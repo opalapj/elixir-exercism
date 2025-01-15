48177095e004beeecc8c373d356acc591fae0b55
Piotr Opała <opalapj@gmail.com>
2025-01-15 21:05:41 +0100

    'New Passport' final

--- new-passport/lib/new_passport.ex
+++ new-passport/lib/new_passport.ex
@@ -1,6 +1,15 @@
 defmodule NewPassport do
   def get_new_passport(now, birthday, form) do
     # Please implement the 'get_new_passport/3' function
+    with {:ok, timestamp} <- enter_building(now),
+         {:ok, manual} <- find_counter_information(now),
+         counter = manual.(birthday),
+         {:ok, checksum} <- stamp_form(timestamp, counter, form) do
+      {:ok, get_new_passport_number(timestamp, counter, checksum)}
+    else
+      {:coffee_break, _} -> {:retry, NaiveDateTime.add(now, 15, :minute)}
+      err -> err
+    end
   end
 
   # Do not modify the functions below
