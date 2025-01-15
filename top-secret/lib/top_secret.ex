f46e6b6333537739b889ff9e1c3a5c7e02d5d789
Piotr Opała <opalapj@gmail.com>
2025-01-15 22:09:58 +0100

    'Top Secret' final

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -1,13 +1,35 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
+  end
+
+  def decode_secret_message_part({op, _, args} = ast, acc) when op in [:def, :defp] do
+    {function_name, function_args} = get_function_name_and_args(args)
+
+    arity = length(function_args)
+    message = String.slice(to_string(function_name), 0, arity)
+
+    {ast, [message | acc]}
   end
 
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    {ast, acc}
+  end
+
+  defp get_function_name_and_args(def_args) do
+    case def_args do
+      [{:when, _, args} | _] -> get_function_name_and_args(args)
+      [{name, _, args} | _] when is_list(args) -> {name, args}
+      [{name, _, args} | _] when is_atom(args) -> {name, []}
+    end
   end
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    ast = to_ast(string)
+    {_, acc} = Macro.prewalk(ast, [], &decode_secret_message_part/2)
+
+    acc
+    |> Enum.reverse()
+    |> Enum.join("")
   end
 end
